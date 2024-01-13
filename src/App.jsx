2c915d2c1a6462e78d31b43cd2e545830c0272d0
Valentina Cuellar Martinez <valencuellar1612@gmail.com>
2024-01-12 20:35:04 -0500

    change today

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,22 @@ function App() {
   const [infoUpdate, setInfoUpdate] = useState()
   const [isDisable, setIsDisable] = useState(false)
   
-  const url = 'https://users-crud.academlo.tech'
+  const url = 'http://localhost:8080'
 
   const [users, getUsers, createUser, deleteUser, updateUser] = useCrud(url)
 
   useEffect(() => {
     getUsers('/users')
   }, [])
-  console.log(users)
 
   const handleNewUser = () => {
     setIsDisable(false)
   }
 
+
   return (
-   <div>
-    <h1>Users</h1>
+   <div className='container'>
+    <h1 className='title'>Users</h1>
     <button onClick={handleNewUser} className='form__btn_1'>Creat new User</button>
     <FormUser
       createUser={createUser}
@@ -35,16 +35,17 @@ function App() {
       isDisable = {isDisable}
       setIsDisable={setIsDisable}
     />
-    <div>
+    <div className='container__2' >
       {
        users?.map(user => (
-          <UserCard 
-            key = {user.id}
-            user = {user}
-            deleteUser = {deleteUser}
-            setInfoUpdate= {setInfoUpdate}
-            setIsDisable={setIsDisable}
-          /> 
+          <div className='users' key = {user.id}>
+            <UserCard 
+              user = {user}
+              deleteUser = {deleteUser}
+              setInfoUpdate= {setInfoUpdate}
+              setIsDisable={setIsDisable}
+           /> 
+          </div>
         ))
       }
     </div>
